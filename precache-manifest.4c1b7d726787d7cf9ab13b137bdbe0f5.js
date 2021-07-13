e646a5950f513e9f86b21bb6404101288147aa7d
Vivi <jiahui1001@yeah.net>
2021-07-13 16:06:04 +0800

    deploy

---
+++ precache-manifest.4c1b7d726787d7cf9ab13b137bdbe0f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f19a8d969e8ee74330f",
+    "url": "/storney-website/css/app.b86f38aa.css"
+  },
+  {
+    "revision": "523f6b7b3be6f131025d72368bf6c043",
+    "url": "/storney-website/index.html"
+  },
+  {
+    "revision": "5f19a8d969e8ee74330f",
+    "url": "/storney-website/js/app.d5614190.js"
+  },
+  {
+    "revision": "66512b822bd035760dcc",
+    "url": "/storney-website/js/chunk-vendors.d788b5c2.js"
+  },
+  {
+    "revision": "1ac9f42eedca7d23d1cab50558f8c769",
+    "url": "/storney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/storney-website/robots.txt"
+  }
+]);
